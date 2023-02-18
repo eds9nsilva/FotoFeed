4d3d226c940cb233083bb9b890586f49d970efef
eds9nsilva <antonioedson99@gmail.com>
2023-02-18 20:51:58 -0300

    feat: redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
-import React from 'react';
-import ApplicationNavigator from './Navigator/Application';
-import Toast from 'react-native-toast-notifications';
-import 'react-native-gesture-handler';
+import React from 'react'
+import ApplicationNavigator from './Navigator/Application'
+import Toast from 'react-native-toast-notifications'
+import { Provider } from 'react-redux'
+import 'react-native-gesture-handler'
+import { PersistGate } from 'redux-persist/lib/integration/react'
+import { persistor, store } from './Store'
 
 const App = () => {
   return (
     <>
-      <ApplicationNavigator />
-      <Toast
-        ref={ref => ((global as any)['toast'] = ref)}
-        placement="bottom"
-        textStyle={{fontSize: 16}}
-        successColor="#28a745"
-        dangerColor="#d0383e"
-        warningColor="#ffc107"
-      />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ApplicationNavigator />
+          <Toast
+            ref={ref => ((global as any)['toast'] = ref)}
+            placement="bottom"
+            textStyle={{ fontSize: 16 }}
+            successColor="#28a745"
+            dangerColor="#d0383e"
+            warningColor="#ffc107"
+          />
+        </PersistGate>
+      </Provider>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ src/Store/index.ts
@@ -0,0 +1,55 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { combineReducers } from 'redux'
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import { configureStore } from '@reduxjs/toolkit'
+
+import { useDispatch } from 'react-redux'
+
+const reducers = combineReducers({
+
+})
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['theme', 'signIn'],
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware => {
+    const middlewares = getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        immutableCheck: { ignoredPaths: ['some.nested.path'] },
+        serializableCheck: { ignoredPaths: ['some.nested.path'] },
+      },
+    })
+
+    if (__DEV__ && !process.env.JEST_WORKER_ID) {
+      const createDebugger = require('redux-flipper').default
+      middlewares.push(createDebugger())
+    }
+
+    return middlewares
+  },
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+
+const persistor = persistStore(store)
+
+export { store, persistor }
